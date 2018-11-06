c749245a0e149adcf36a27708342ccf584236f14
themayflyman <xuziwei100@gmail.com>
2018-11-06 17:35:09 +0800

    add proxy support

--- downloader.py
+++ downloader.py
@@ -4,16 +4,19 @@
 import aiohttp
 
 
-class Downloader():
+class Downloader:
     """
     """
     def __init__(self, loop):
         self._timeout = 0
 
         self.session = loop.run_until_complete(self._create_session())
+
         self.proxy = {'http': None,
                       'https': None}
+
         self.headers = {}
+        self.proxy_pool = None
 
     async def _create_session(self):
         session = aiohttp.ClientSession()
@@ -30,9 +33,11 @@ class Downloader():
         else:
             raise ValueError('timeout must be a integer.')
 
+    def set_proxy(self, protocol):
+        self.proxy[protocol] = self.proxy_pool.get(protocol)
+
     async def download(self, node):
-        proxy_type = node.get('proxy_type')
-        proxy = self.proxy.get(proxy_type)
+        proxy = self.proxy.get(node.get('protocol'))
         async with self.session.request(method=node.get('method'), 
                                         url=node.get('url'), 
                                         data=node.get('data'),
